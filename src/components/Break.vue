8c098425525369de5d0d0c42f7a7df4a796947e1
hignz <higz-14@live.com>
2020-01-26 21:09:24 +0000

    added break component

---
+++ src/components/Break.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-menu offset-x offset-y="" absolute>
+    <template v-slot:activator="{ on }">
+      <p class="primary--text pt-4" v-on="on">
+        Break: {{ breakFormatted(period.breakLength) }}
+      </p>
+    </template>
+    <v-list dense>
+      <v-list-item
+        v-for="(item, index) in items"
+        :key="index"
+        @click="navigateTo(item.url)"
+      >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+        <v-list-item-icon>
+          <v-icon>mdi-library</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Room booking',
+          url: 'https://libreserve.itsligo.ie/'
+        }
+      ]
+    };
+  },
+  props: {
+    period: {
+      type: Object
+    }
+  },
+  methods: {
+    breakFormatted(length) {
+      return length >= 60 ? `${length / 60} hour` : `${length} minutes`;
+    },
+    navigateTo(url) {
+      window.open(url);
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/components/TimetableItem.vue
+++ src/components/TimetableItem.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
     <div v-for="(period, i) in day" :key="i" class="text-center">
-      <p class="mt-4 mb-1 body-2">
-        {{ period.startTime }} - {{ period.endTime }}
-      </p>
-      <p class="mb-1 body-2" v-if="period.name.length > 1">
-        {{ period.name }}
-      </p>
-      <p class="mb-1 body-2">{{ period.room.split(' -')[0] }}</p>
-      <p class="body-2 grey--text" v-if="period.teacher.length">
-        {{ period.teacher }}
-      </p>
+      <Break v-if="period.break" :period="period" />
+      <div v-else>
+        <p class="mt-4 mb-1 body-2">
+          {{ period.startTime }} - {{ period.endTime }}
+        </p>
+        <p class="mb-1 body-2" v-if="period.name && period.name.length > 1">
+          {{ period.name }}
+        </p>
+        <p class="mb-1 body-2">{{ period.room.split(' -')[0] }}</p>
+        <p
+          class="body-2 grey--text"
+          v-if="period.teacher && period.teacher.length"
+        >
+          {{ period.teacher }}
+        </p>
+      </div>
       <v-divider v-if="i !== day.length - 1" />
     </div>
   </div>
 </template>
 
 <script>
+import Break from './Break';
+
 export default {
+  components: {
+    Break
+  },
   props: {
     day: {
       type: Array,
