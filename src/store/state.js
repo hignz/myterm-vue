e252ccfdab19248946c760376074cf2c7ed95126
hignz <higz-14@live.com>
2020-01-26 21:10:00 +0000

    fixed dark mode pref not be respected

--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,6 @@
 export default {
   lastTimetableVisited: localStorage.getItem('courseQuery') || null,
-  darkMode: JSON.parse(localStorage.getItem('darkMode')) || true,
+  darkMode: JSON.parse(localStorage.getItem('darkMode')) || null,
   loading: true,
   accentedBorders: JSON.parse(localStorage.getItem('accentedBorders')) || true
 };
