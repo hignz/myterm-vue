618d424287fb19e7c38401fea23f5ff0bdc170bd
hignz <higz-14@live.com>
2020-01-27 00:51:15 +0000

    PieChart label color reactive

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,8 +1,9 @@
 <script>
-import { Pie } from 'vue-chartjs';
+import { Doughnut } from 'vue-chartjs';
+import { mapState } from 'vuex';
 
 export default {
-  extends: Pie,
+  extends: Doughnut,
   props: {
     chartData: {
       type: Array,
@@ -22,13 +23,13 @@ export default {
             label: 'Skills',
             borderWidth: 1,
             backgroundColor: [
-              'rgba(255, 99, 132, 0.8)',
-              'rgba(54, 162, 235, 0.8)',
-              'rgba(255, 206, 86, 0.8)',
-              'rgba(75, 192, 192, 0.8)',
-              'rgba(153, 102, 255, 0.8)',
-              'rgba(255, 159, 64, 0.8)',
-              'rgba(50, 180, 64, 0.8)'
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(50, 180, 64, 1)'
             ],
             data: this.chartData
           }
@@ -36,9 +37,22 @@ export default {
       },
       {
         responsive: true,
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
+        legend: {
+          labels: {
+            fontColor: this.darkMode ? '#ffffff' : 'black'
+          }
+        }
       }
     );
+  },
+  beforeDestroy() {
+    if (this.$data._chart) {
+      this.$data._chart.destroy();
+    }
+  },
+  computed: {
+    ...mapState(['darkMode'])
   }
 };
 </script>
