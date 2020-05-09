506d7358af49045ec322a9e9c71c25092caf6526
hignz <higz-14@live.com>
2020-05-09 01:23:47 +0100

    using new api url

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { cacheAdapterEnhancer } from 'axios-extensions';
 
 const http = axios.create({
-  baseURL: 'https://itsligo-utils.herokuapp.com/api',
+  baseURL: 'https://myterm.herokuapp.com/api',
   adapter: cacheAdapterEnhancer(axios.defaults.adapter)
 });
 
