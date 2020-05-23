defb2affe0d32b9a397e47880425ac679ff603bf
hignz <higz-14@live.com>
2020-05-23 17:18:48 +0100

    Saved.vue: now using state to handle saved timetables

--- src/components/pages/Saved.vue
+++ src/components/pages/Saved.vue
@@ -9,39 +9,36 @@
         md="6"
       >
         <v-card :class="accentedBorder" flat>
-          <!-- <v-card-text> </v-card-text> -->
-          <v-subheader>Courses</v-subheader>
-          <v-list nav two-line subheader dense>
-            <v-list-item-group>
-              <v-list-item
-                v-for="(course, index) in savedCourses"
-                :key="index"
-                @click="openTimetable(course)"
-              >
-                <v-list-item-content>
-                  <v-list-item-title>{{ course.title }}</v-list-item-title>
-                  <v-list-item-subtitle class="mt-1">
-                    Semester:
-                    {{ parseInt(course.sem, 10) + 1 }}
-                  </v-list-item-subtitle>
-                  <v-list-item-subtitle class="overline mt-2">
-                    {{ course.college }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-action class="mt-1">
-                  <v-btn icon @click="openDeleteDialog(course)" @click.stop>
-                    <v-icon small>
-                      mdi-delete
-                    </v-icon>
-                  </v-btn>
-                </v-list-item-action>
-              </v-list-item>
-            </v-list-item-group>
+          <v-list nav three-line dense>
+            <v-subheader>COURSES</v-subheader>
+            <v-list-item
+              v-for="(course, index) in savedCourses"
+              :key="index"
+              @click="openTimetable(course)"
+            >
+              <v-list-item-content>
+                <v-list-item-title>{{ course.title }}</v-list-item-title>
+                <v-list-item-subtitle>
+                  Semester:
+                  {{ parseInt(course.sem, 10) + 1 }}
+                </v-list-item-subtitle>
+                <v-list-item-subtitle class="overline mt-1">
+                  {{ course.college }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action class="mt-1">
+                <v-btn icon @click="openDeleteDialog(course)" @click.stop>
+                  <v-icon small>
+                    mdi-delete
+                  </v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
           </v-list>
         </v-card>
       </v-col>
       <v-col v-else class="text-center mt-4" cols="12" sm="12" md="6">
-        <v-icon class="mb-4 grey--text" x-large>
+        <v-icon class="mb-4 grey--text" size="50">
           mdi-heart-broken
         </v-icon>
         <p class="grey--text">
@@ -76,21 +73,22 @@
 import AppBar from '@/components/shared/AppBar';
 import genericMetaInfo from '@/mixins/genericMetaInfo';
 import accentedBorder from '@/mixins/accentedBorder';
+import { mapState, mapActions } from 'vuex';
 
 export default {
   components: {
     AppBar
   },
-  mixins: [genericMetaInfo, accentedBorder],
+  mixins: [accentedBorder, genericMetaInfo],
   data: () => ({
-    savedCourses: null,
     showDialog: false,
     selectedCourse: null
   }),
-  mounted() {
-    this.savedCourses = JSON.parse(localStorage.getItem('savedCourses'));
+  computed: {
+    ...mapState(['savedCourses'])
   },
   methods: {
+    ...mapActions(['removeTimetable']),
     openTimetable(course) {
       this.$router.push({
         path: '/timetable',
@@ -102,10 +100,7 @@ export default {
       });
     },
     deleteCourse() {
-      this.savedCourses = this.savedCourses.filter(
-        el => el !== this.selectedCourse
-      );
-      localStorage.setItem('savedCourses', JSON.stringify(this.savedCourses));
+      this.removeTimetable(this.selectedCourse);
 
       this.showDialog = false;
 
@@ -114,7 +109,6 @@ export default {
         position: 'bottom-center'
       });
     },
-
     openDeleteDialog(course) {
       this.showDialog = true;
       this.selectedCourse = course;
