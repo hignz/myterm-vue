964907416199040c75806779692d43e564461e9c
hignz <higz-14@live.com>
2020-05-26 04:27:21 +0100

    fixed dark mode not sticking

--- src/store/state.js
+++ src/store/state.js
@@ -2,7 +2,7 @@ export default {
   accentedBorders: JSON.parse(localStorage.getItem('accentedBorders')) || false,
   currentClass: null,
   currentTimetable: {},
-  darkMode: JSON.parse(localStorage.getItem('darkMode')) || true,
+  darkMode: JSON.parse(localStorage.getItem('darkMode')) || null,
   fetching: false,
   savedCourses: JSON.parse(localStorage.getItem('savedCourses')) || [],
   showWeekends: JSON.parse(localStorage.getItem('showWeekends')) || null,
