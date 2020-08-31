2b605ccca82bd5b11d3cd06197bd5144b9345745
hignz <higz-14@live.com>
2020-08-31 17:42:19 +0100

    AppBar.vue: added showBackButton prop

--- src/components/shared/AppBar.vue
+++ src/components/shared/AppBar.vue
@@ -1,7 +1,12 @@
 <template>
   <v-app-bar hide-on-scroll flat app>
     <v-row justify="center" align="center">
-      <v-btn text class="font-weight-bold" @click="onBack()">
+      <v-btn
+        v-if="showBackButton"
+        text
+        class="font-weight-bold"
+        @click="onBack()"
+      >
         <v-icon class="mr-2">{{ mdiArrowLeft }}</v-icon>
         {{ title }}
       </v-btn>
@@ -23,7 +28,11 @@ export default {
   props: {
     title: {
       type: String,
-      required: true
+      default: 'MyTerm'
+    },
+    showBackButton: {
+      type: Boolean,
+      default: true
     }
   },
   data() {
