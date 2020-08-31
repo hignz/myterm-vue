2a902d2ee87d3e15a465bef7c6d8015a90cb3ca7
hignz <higz-14@live.com>
2020-08-31 17:39:06 +0100

    Saved.vue: wrap text, various style changes

--- src/components/pages/Saved.vue
+++ src/components/pages/Saved.vue
@@ -1,35 +1,49 @@
 <template>
   <v-container>
-    <AppBar title="Saved" />
+    <AppBar v-if="$vuetify.breakpoint.smAndDown" title="Saved" />
     <v-row justify="center">
       <v-col
         v-if="savedCourses && savedCourses.length"
         cols="12"
         sm="12"
         md="6"
+        lg="6"
       >
-        <v-card :class="accentedBorder" flat>
-          <v-list nav three-line dense>
-            <v-subheader>COURSES</v-subheader>
+        <v-card outlined flat>
+          <v-list three-line subheader dense nav>
+            <v-subheader class="font-weight-bold caption mt-2"
+              >TIMETABLES <v-spacer></v-spacer
+              ><span v-if="savedCourses.length > 1" class="mr-3">{{
+                savedCourses.length
+              }}</span></v-subheader
+            >
+
             <v-list-item
               v-for="(course, index) in savedCourses"
               :key="index"
               @click="openTimetable(course)"
             >
               <v-list-item-content>
-                <v-list-item-title>{{ course.title }}</v-list-item-title>
-                <v-list-item-subtitle>
+                <v-list-item-title class="mb-1 mb-md-0">{{
+                  course.title
+                }}</v-list-item-title>
+                <v-list-item-subtitle
+                  v-if="!course.multiple"
+                  class="caption mb-2 mb-md-0"
+                >
                   Semester:
-                  {{ parseInt(course.sem, 10) + 1 }}
+                  <span class="font-weight-bold">{{
+                    parseInt(course.sem, 10) + 1
+                  }}</span>
                 </v-list-item-subtitle>
-                <v-list-item-subtitle class="overline mt-1">
+                <v-list-item-subtitle class="caption">
                   {{ course.college }}
                 </v-list-item-subtitle>
               </v-list-item-content>
-              <v-list-item-action class="mt-1">
-                <v-btn icon @click="openDeleteDialog(course)" @click.stop>
-                  <v-icon small>
-                    mdi-delete
+              <v-list-item-action>
+                <v-btn icon @click.stop="openDeleteDialog(course)">
+                  <v-icon size="20">
+                    {{ mdiDelete }}
                   </v-icon>
                 </v-btn>
               </v-list-item-action>
@@ -39,7 +53,7 @@
       </v-col>
       <v-col v-else class="text-center mt-4" cols="12" sm="12" md="6">
         <v-icon class="mb-4 grey--text" size="50">
-          mdi-heart-broken
+          {{ mdiHeartBroken }}
         </v-icon>
         <p class="grey--text">
           You have no saved courses.
@@ -49,18 +63,25 @@
         </p>
       </v-col>
     </v-row>
-    <v-dialog v-model="showDialog" :width="400">
+    <v-dialog v-model="showDialog" :width="425">
       <v-card>
-        <v-card-title>Remove course</v-card-title>
-        <v-card-text>
-          Are you sure you?
+        <v-card-title class="subtitle-1 text-uppercase"
+          >Remove course</v-card-title
+        >
+        <v-card-text class="mt-4 caption">
+          <p>
+            Are you sure you want to remove
+            <span v-if="selectedTimetable" class="white--text font-weight-bold">
+              {{ selectedTimetable.title }} </span
+            >?
+          </p>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn text @click="showDialog = !showDialog">
-            Close
+          <v-btn class="mr-2" text @click="showDialog = !showDialog">
+            Cancel
           </v-btn>
-          <v-btn color="error" @click="deleteCourse()">
+          <v-btn color="error darken-1" @click="deleteCourse()">
             Remove
           </v-btn>
         </v-card-actions>
@@ -72,17 +93,19 @@
 <script>
 import AppBar from '@/components/shared/AppBar';
 import genericMetaInfo from '@/mixins/genericMetaInfo';
-import accentedBorder from '@/mixins/accentedBorder';
 import { mapState, mapActions } from 'vuex';
+import { mdiDelete, mdiHeartBroken } from '@mdi/js';
 
 export default {
   components: {
     AppBar
   },
-  mixins: [accentedBorder, genericMetaInfo],
+  mixins: [genericMetaInfo],
   data: () => ({
     showDialog: false,
-    selectedCourse: null
+    selectedTimetable: null,
+    mdiDelete,
+    mdiHeartBroken
   }),
   computed: {
     ...mapState(['savedCourses'])
@@ -100,19 +123,23 @@ export default {
       });
     },
     deleteCourse() {
-      this.removeTimetable(this.selectedCourse);
+      this.removeTimetable(this.selectedTimetable).then(() => {
+        this.showDialog = false;
 
-      this.showDialog = false;
-
-      this.$toast.error('Removed from favourites', {
-        timeout: 2000,
-        position: 'bottom-center'
+        this.$toast.error('Timetable removed');
       });
     },
     openDeleteDialog(course) {
+      this.selectedTimetable = course;
       this.showDialog = true;
-      this.selectedCourse = course;
     }
   }
 };
 </script>
+
+<style>
+.v-list-item__title,
+.v-list-item__subtitle {
+  white-space: normal;
+}
+</style>
