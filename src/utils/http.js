7c1a6667ba1240367f1273148420a2710827211f
hignz <higz-14@live.com>
2020-08-31 20:04:28 +0100

    fixed axios baseUrl

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { cacheAdapterEnhancer } from 'axios-extensions';
 
 const http = axios.create({
-  baseURL: 'https://myterm.herokuapp.com/api',
+  baseURL: 'https://myterm.herokuapp.com/v1',
   adapter: cacheAdapterEnhancer(axios.defaults.adapter)
 });
 
