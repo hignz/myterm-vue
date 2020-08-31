8c2623c42c9df059b356a0f2169fdd32f586cc55
hignz <higz-14@live.com>
2020-08-31 17:41:57 +0100

    vuetify.js: cleanup

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,40 +3,32 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
-if (!localStorage.getItem('accentColor')) {
-  localStorage.setItem('accentColor', '#41D1AB');
-}
-if (!localStorage.getItem('darkMode')) {
-  localStorage.setItem('darkMode', true);
-}
-if (!localStorage.getItem('accentedBorders')) {
-  localStorage.setItem('accentedBorders', false);
-}
-
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg'
+  },
   theme: {
     dark: true,
     options: {
-      customProperties: true
+      customProperties: true,
+      variations: true
     },
     themes: {
       light: {
-        primary: localStorage.getItem('accentColor'),
-        secondary: '#424242',
-        accent: '#82B1FF',
+        primary: JSON.parse(localStorage.getItem('vuex'))
+          ? JSON.parse(localStorage.getItem('vuex')).accentColor
+          : '#72DDF7',
         error: '#FF5252',
-        info: '#2196F3',
         success: '#41D1AB',
-        warning: '#FFC107'
+        background: '#0D0F11'
       },
       dark: {
-        primary: localStorage.getItem('accentColor'),
-        secondary: '#424242',
-        accent: '#82B1FF',
+        primary: JSON.parse(localStorage.getItem('vuex'))
+          ? JSON.parse(localStorage.getItem('vuex')).accentColor
+          : '#72DDF7',
         error: '#FF5252',
-        info: '#2196F3',
         success: '#41D1AB',
-        warning: '#FFC107'
+        background: '#0D0F11'
       }
     }
   }
