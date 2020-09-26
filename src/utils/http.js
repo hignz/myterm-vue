524e4aa272dee312e25e09f45c6f25f4a7c7ea42
hignz <higz-14@live.com>
2020-09-26 06:41:36 +0100

    fix prettier error

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import { cacheAdapterEnhancer } from 'axios-extensions';
 
 const http = axios.create({
   baseURL: 'http://localhost:3000/v1',
-  adapter: cacheAdapterEnhancer(axios.defaults.adapter)
+  adapter: cacheAdapterEnhancer(axios.defaults.adapter),
 });
 
 export default http;
