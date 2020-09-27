4c426b8091d71e60b7f6b9953e90c7ccdf3574f3
hignz <higz-14@live.com>
2020-09-27 23:36:40 +0100

    refactor: create quick-links mixin and quick-link component

---
+++ src/components/shared/QuickLinkItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-list-item @click="openLink(link.url)">
+      <v-icon v-if="showIcon" class="ml-2 mr-6 my-4" color="primary">
+        {{ link.icon }}
+      </v-icon>
+      <v-list-item-title>{{ link.title }}</v-list-item-title>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Object,
+      default: () => {},
+    },
+    showIcon: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
---
+++ src/mixins/quickLinks.js
@@ -0,0 +1,63 @@
+import {
+  mdiSchool,
+  mdiMail,
+  mdiGoogleClassroom,
+  mdiDesktopClassic,
+  mdiLibraryShelves,
+  mdiViewDashboard,
+  mdiCounter,
+} from '@mdi/js';
+
+export default {
+  data() {
+    return {
+      links: [
+        [
+          {
+            title: 'Email',
+            url: 'http://outlook.com/mail.itsligo.ie',
+            icon: mdiMail,
+          },
+          {
+            title: 'Moodle',
+            url: 'https://vle.itsligo.ie/my/index.php',
+            icon: mdiSchool,
+          },
+          {
+            title: 'Room Booking',
+            url: 'https://libreserve.itsligo.ie/',
+            icon: mdiGoogleClassroom,
+          },
+          {
+            title: 'Virtual Desktop',
+            url: 'https://vdesktop.itsligo.ie/Citrix/SligoWeb/',
+            icon: mdiDesktopClassic,
+          },
+          {
+            title: 'Library',
+            url: 'https://library.itsligo.ie/',
+            icon: mdiLibraryShelves,
+          },
+          {
+            title: 'Grades/Registration/Payments',
+            url:
+              'https://ssb.ancheim.ie/its/app/twbkwbis.P_GenMenu?name=homepage',
+            icon: mdiCounter,
+          },
+          {
+            title: 'Student Hub',
+            url: 'https://www.itsligo.ie/student-hub/',
+            icon: mdiViewDashboard,
+          },
+        ],
+        [],
+        [
+          {
+            title: 'Moodle',
+            url: 'https://moodle.lit.ie/',
+          },
+        ],
+      ],
+    };
+  },
+};
