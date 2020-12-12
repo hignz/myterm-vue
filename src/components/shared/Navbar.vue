aedd2635a0f6c485dd1ac1ecf6350332dd6e39ee
hignz <higz-14@live.com>
2020-12-12 01:00:50 +0000

    add christmas lights

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,16 +1,15 @@
 <template>
   <v-app-bar class="elevation-0" app dense extended extension-height="7">
+    <ChristmasLights />
     <v-toolbar-title class="mr-14 title">
       <span>My<span class="primary--text">Term</span></span>
     </v-toolbar-title>
     <v-btn-toggle group active-class="active">
-      <NavigationItem
-        v-for="item in items"
-        :key="item.text"
-        :item="item"
-        :show-icon="false"
-      />
+      <template v-for="item in items">
+        <NavigationItem :key="item.text" :item="item" :show-icon="false" />
+      </template>
     </v-btn-toggle>
+
     <template v-slot:extension>
       <ProgressBar style="top: -4px" />
     </template>
@@ -21,9 +20,11 @@
 import NavigationItem from '@/components/shared/NavigationItem';
 import navigationItems from '@/mixins/navigationItems';
 import ProgressBar from '@/components/shared/ProgressBar';
+import ChristmasLights from './ChristmasLights';
 
 export default {
   components: {
+    ChristmasLights,
     NavigationItem,
     ProgressBar,
   },
