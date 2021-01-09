c839ba57d3309ac861a4fab2509d2c2daf076951
hignz <higz-14@live.com>
2021-01-09 00:16:55 +0000

    meta data changes

--- src/mixins/timetableMetaInfo.js
+++ src/mixins/timetableMetaInfo.js
@@ -7,32 +7,33 @@ export default {
     }
 
     const { title, college } = this.timetable;
-    const defaultKeywords = ['Timetables, IT Sligo, Limerick IT'];
-    const description = `The timetable for course ${title} of ${college}`;
     const ogTitle = `${title} timetable`;
+    const description = `The timetable for course ${title} of ${college}`;
+    const keywords =
+      'timetable, IT Sligo, college, course, Limerick IT, IT Sligo timetable, Limerick IT timetable, student timetables, education, university';
 
     return {
       title: `${title} · Timetable - MyTerm`,
       meta: [
         {
-          name: 'description',
-          content: `${title}, ${college}, ${description}, MyTerm.me allows users to view and save timetables of IT Sligo and Limerick IT`,
+          name: 'og:title',
+          content: ogTitle,
         },
         {
-          name: 'keywords',
-          content: defaultKeywords,
+          name: 'description',
+          content: `${title}, ${college}, ${description}, MyTerm.me allows users to view and save timetables of IT Sligo and Limerick IT`,
         },
         {
           name: 'og:description',
           content: description,
         },
         {
-          name: 'og:title',
-          content: ogTitle,
-        },
-        {
           name: 'twitter:description',
           content: description,
+        },
+        {
+          name: 'keywords',
+          content: keywords,
         },
       ],
     };
