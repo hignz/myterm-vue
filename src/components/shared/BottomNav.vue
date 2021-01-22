45fa0110dc663d3732d199860bb3fbfa5a88d35b
hignz <higz-14@live.com>
2021-01-22 16:11:06 +0000

    experimental fix for iphone gesture pill blocking botton nav

--- src/components/shared/BottomNav.vue
+++ src/components/shared/BottomNav.vue
@@ -1,6 +1,7 @@
 <template>
   <v-bottom-navigation
     class="elevation-0"
+    :class="{ 'safe-area': isIos }"
     grow
     height="50"
     app
@@ -24,11 +25,32 @@ export default {
     NavigationItem,
   },
   mixins: [navigationItems],
+  computed: {
+    isIos() {
+      console.log(navigator.platform);
+      return /iPad|iPhone|iPod/.test(navigator.platform);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .v-btn:not(.v-btn--text):not(.v-btn--outlined):hover:before {
   opacity: 0 !important;
+}
+
+.safe-area.v-bottom-navigation {
+  height: auto !important;
+}
+
+.safe-area >>> .v-btn__content {
+  padding-top: 10px !important;
+  padding-bottom: 10px !important;
+  margin-bottom: constant(
+    safe-area-inset-bottom
+  ) !important; /* move footer up on iPhone X - iOS 11.0 */
+  margin-bottom: env(
+    safe-area-inset-bottom
+  ) !important; /* move footer up on iPhone X - iOS 11.2 */
 }
 </style>
